a7ba47e330e5ec50a38ea6e806ad9406282f7826
Dongkeun <hongdongk@naver.com>
2022-03-04 19:08:22 +0900

    Coin_update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+
   const [toDo, setToDo] = useState("");
   const [toDos, setToDos] = useState([]);
   const onChange = (event) => setToDo(event.target.value);
@@ -12,6 +15,15 @@ function App() {
     setToDos((currentArray) => [toDo, ...currentArray]);   //...currentarray= 기존배열요소에 추가하기
     setToDo("");
   };
+  
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
 
   return (
     <div>
@@ -20,10 +32,13 @@ function App() {
         <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..."/>
         <button>Add To Do</button>
       </form>
-      <hr />
+
       <ul>
         {toDos.map((item, index) => (<li key={index}>{item}</li>))}
       </ul>
+      
+      <h2>The Coins! {loading ? "" : `(${coins.length})`}</h2>
+      {loading ? (<strong>Loading...</strong>) : (<select>{coins.map((coin) => (<option>{coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD</option>))}</select> )}
     </div>
   );
 }
