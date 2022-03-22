9e5e7a2cd40544125260a1f0c8fea3e605c799fc
Dongkeun <64244383+HongDongk@users.noreply.github.com>
2022-03-22 16:30:44 +0900

    Lastupdate

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,27 +1,28 @@
+import styles from "./Info.module.css";
+
+
 function Info({background_image_original, medium_cover_image, url, title_long, rating, runtime, genres, download_count}) {
   return (
     <div>
-            <img src={background_image_original} />           
-            <div>
-                <img  src={medium_cover_image} />
-                <div>
-                    <h1><a href={url} target="_blank">{title_long}</a></h1>
-                    <ul>
-                        <li>Rating {rating}</li>
-                        <li>Runtime {runtime}</li>
-                        <li>Download {download_count}</li>
-                        <li>
-                            Genres
-                            <ul>
-                                {genres.map(genre => <li>{genre}</li>)}
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
+        <img className={styles.bg} src={background_image_original} />           
+        <div className={styles.show}>
+            <img className={styles.img} src={medium_cover_image} />
+            <div className={styles.textbox}>
+                <h1 className={styles.title}><a href={url} target="_blank">{title_long}</a></h1>
+                <ul className={styles.bigul}>
+                    <li>Rating {rating}</li>
+                    <li>Runtime {runtime}</li>
+                    <li>Download {download_count}</li>
+                    <li>Genres
+                        <ul className={styles.smul}>
+                            {genres.map(genre => <li>{genre}</li>)}
+                        </ul>
+                    </li>
+                </ul>
             </div>
         </div>
-
-  );
+    </div>
+    );
 }
 
 
